dfb607aa378700910d30681afed195bdf5a36e4c
Alan Klein <alan@alanklein.dev>
2019-11-14 21:48:27 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Timer from "./components/StatefulComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello World!</h2>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
